f799fc3399dd1bf6db87cf21707ca11b637fd081
Snir Nahari <snirdavid994@gmail.com>
2021-03-23 16:24:31 +0200

    cd imp

--- ex1.c
+++ ex1.c
@@ -5,19 +5,21 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-typedef struct jobRec {
+//defining struct for job Record
+struct jobRec {
     char jobName[100];
     char *status;
     int background;
     pid_t pid;
 };
 
-int splitString(char **jobs, char *input) {
+//getting user input and splitting into tokens using whitespace as a delim
+int splitString(char **currentJob, char *input) {
     char *jobToken;
     int commandLen;
     jobToken = strtok(input, " ");
     for (int i = 0; jobToken != NULL; i++) {
-        jobs[i] = jobToken;
+        currentJob[i] = jobToken;
 //        printf("%s\n", jobs[i]);
         jobToken = strtok(NULL, " ");
         commandLen = i;
@@ -40,7 +42,51 @@ void historyComm(struct jobRec history[100], int commandNumber) {
     }
 }
 
-void cd() {}
+void cdCheck(char *path, char prevPath[100]) {
+    char currentDir[100];
+    //checking special chars in cd command (~ or -)
+    if (strcmp(path, "-") == 0) {
+        //setting previous path as path for chdir command
+        strcpy(path, prevPath);
+    } else if (strstr(path, "~")) { //checking if string contain ~
+        //checking if this the only char
+        if (strcmp(path, "~") == 0) {
+            //changing path to home dir
+            strcpy(path,getenv("HOME"));
+        } else {
+            if (getcwd(currentDir, sizeof(currentDir)) == NULL) {
+                printf("An error occurred\n");
+            } else {
+                //concreting home dir to path
+                strcpy(currentDir,getenv("HOME"));
+                strcat(currentDir, ++path);
+                strcpy(--path, currentDir);
+//                printf("%s\n", path);
+            }
+        }
+    }
+}
+
+void cd(char *path, int commandLen, char prevPath[100]) {
+    char currentDir[100];
+    if (getcwd(currentDir, sizeof(currentDir)) == NULL) {
+        printf("An error occurred\n");
+    } else {
+        if (commandLen > 1) {
+            printf("Too many arguments\n");
+        } else {
+            cdCheck(path, prevPath);
+            int cd = chdir(path);
+            if (cd == -1) {
+                printf("chdir failed\n");
+            } else {
+                strcpy(prevPath, currentDir);
+//                printf("%s\n", prevPath);
+            }
+        }
+    }
+}
+
 
 void exitShell() {
     exit(0);
@@ -51,12 +97,10 @@ void exitShell() {
 
 int main() {
     //initialize variables
-    int commandNumber = 0;
-    char *currentJob[100];
-    char *builtInFunc[] = {"jobs", "history", "cd", "exit"};
-    int status, commandLen;
-    pid_t pid;
+    int commandNumber = 0, status, commandLen;
+    char *currentJob[100], prevPath[100], *builtInFunc[] = {"jobs", "history", "cd", "exit"};
     struct jobRec history[100];
+    pid_t pid;
 
     //shell loop
     while (1) {
@@ -66,6 +110,7 @@ int main() {
         printf("$ ");
         fflush(stdout);
         //getting command from user
+        //TODO - dealing with "empty" command
         scanf(" %[^\n]s", input);
         //splitting command into tokens and getting back input len
         commandLen = splitString(currentJob, input);
@@ -87,7 +132,7 @@ int main() {
                         historyComm(history, commandNumber);
                         break;
                     case 2:
-                        cd();
+                        cd(currentJob[1], commandLen, prevPath);
                         break;
                     case 3:
                         exitShell();
@@ -125,11 +170,11 @@ int main() {
                 //checking if & last char in user input - if it isn't, waiting for child process
                 if (strcmp(currentJob[commandLen], "&") != 0) {
                     int waited = wait(&status);
-                    printf(" waiting\n");
-                    fflush(stdout);
+//                    printf(" waiting\n");
+//                    fflush(stdout);
                 } else {
-                    printf("not waiting\n");
-                    fflush(stdout);
+//                    printf("not waiting\n");
+//                    fflush(stdout);
                     history[commandNumber].background = 1;
                     sleep(1);
                 }
